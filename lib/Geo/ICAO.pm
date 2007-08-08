2d261aac87507b15c6c8dd6f982ccaa2c226a5a0
Jérôme Quelin <jquelin@gmail.com>
2007-08-08 10:25:32 +0000

    r5731@merlin:  jquelin | 2007-08-08 12:17:23 +0200  initial files created by module-starter

---
+++ lib/Geo/ICAO.pm
@@ -0,0 +1,101 @@
+package Geo::ICAO;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Geo::ICAO - The great new Geo::ICAO!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Geo::ICAO;
+
+    my $foo = Geo::ICAO->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin at cpan.org> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to
+C<bug-geo-icao at rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Geo-ICAO>.
+I will be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Geo::ICAO
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Geo-ICAO>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Geo-ICAO>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Geo-ICAO>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Geo-ICAO>
+
+=back
+
+=head1 ACKNOWLEDGEMENTS
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2007 Jerome Quelin, all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1; # End of Geo::ICAO
