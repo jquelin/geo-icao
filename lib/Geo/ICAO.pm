b8eaf4f420f8dec991739f40add1d450b5dd3711
Jérôme Quelin <jquelin@gmail.com>
2007-08-08 16:18:19 +0000

    r5742@merlin:  jquelin | 2007-08-08 13:46:58 +0200  reorganized pod + license reminder

--- lib/Geo/ICAO.pm
+++ lib/Geo/ICAO.pm
@@ -1,71 +1,66 @@
+#
+# This file is part of Geo::ICAO
+# Copyright (c) 2007 Jerome Quelin, all rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the same terms as Perl itself.
+#
+
 package Geo::ICAO;
 
 use warnings;
 use strict;
 
-=head1 NAME
-
-Geo::ICAO - The great new Geo::ICAO!
-
-=head1 VERSION
-
-Version 0.01
-
-=cut
 
 our $VERSION = '0.01';
 
+
+1;
+__END__
+
+=head1 NAME
+
+Geo::ICAO - Airport and ICAO codes lookup
+
+
+
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use Geo::ICAO;
-
-    my $foo = Geo::ICAO->new();
     ...
+
+
 
 =head1 EXPORT
 
 A list of functions that can be exported.  You can delete this section
 if you don't export anything, such as for a purely object-oriented module.
 
+
+
 =head1 FUNCTIONS
 
 =head2 function1
 
-=cut
 
-sub function1 {
-}
-
-=head2 function2
-
-=cut
-
-sub function2 {
-}
-
-=head1 AUTHOR
-
-Jerome Quelin, C<< <jquelin at cpan.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to
-C<bug-geo-icao at rt.cpan.org>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Geo-ICAO>.
-I will be notified, and then you'll automatically be notified of progress on
-your bug as I make changes.
+Please report any bugs or feature requests to C<< < bug-geo-icao at
+rt.cpan.org> >>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Geo-ICAO>.  I will be
+notified, and then you'll automatically be notified of progress on your
+bug as I make changes.
 
-=head1 SUPPORT
 
-You can find documentation for this module with the perldoc command.
 
-    perldoc Geo::ICAO
+=head1 SEE ALSO
 
-You can also look for information at:
+C<Geo::ICAO> development takes place on L<http://geo-icao.googlecode.com>
+- feel free to join us.
+
+
+You can also look for information on this module at:
 
 =over 4
 
@@ -81,21 +76,22 @@ L<http://cpanratings.perl.org/d/Geo-ICAO>
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Geo-ICAO>
 
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Geo-ICAO>
-
 =back
 
-=head1 ACKNOWLEDGEMENTS
+
+
+=head1 AUTHOR
+
+Jerome Quelin, C<< <jquelin at cpan.org> >>
+
+
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2007 Jerome Quelin, all rights reserved.
+Copyright (c) 2007 Jerome Quelin, all rights reserved.
 
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
 
 =cut
-
-1; # End of Geo::ICAO
