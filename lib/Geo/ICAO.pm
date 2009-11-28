97dc89519156d86e07ede25ced0c4f41bf961b00
Jérôme Quelin <jquelin@gmail.com>
2009-11-28 16:34:34 +0100

    pod update

--- lib/Geo/ICAO.pm
+++ lib/Geo/ICAO.pm
@@ -598,58 +598,35 @@ return both the airport name and its location (if known).
 
 
 
-=head1 BUGS
-
-Please report any bugs or feature requests to C<< < bug-geo-icao at
-rt.cpan.org> >>, or through the web interface at
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Geo-ICAO>.  I will be
-notified, and then you'll automatically be notified of progress on your
-bug as I make changes.
-
-
-
 =head1 SEE ALSO
 
-C<Geo::ICAO> development takes place on L<http://geo-icao.googlecode.com>
-- feel free to join us.
-
-
-You can also look for information on this module at:
+You can look for information on this module at:
 
 =over 4
 
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Geo-ICAO>
+
+=item * See open / report bugs
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Geo-ICAO>
+
+=item * Git repository
+
+L<http://github.com/jquelin/geo-icao>
+
 =item * AnnoCPAN: Annotated CPAN documentation
 
-L<http://annocpan.org/dist/Geo-ICAO>
+L<http://annocpan.org/dist/Geo-ICA>
 
 =item * CPAN Ratings
 
 L<http://cpanratings.perl.org/d/Geo-ICAO>
 
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Geo-ICAO>
-
 =back
 
-
-
-=head1 AUTHOR
-
-Jerome Quelin, C<< <jquelin at cpan.org> >>
-
-
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright (c) 2007 Jerome Quelin, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-
 =cut
-
 
 __DATA__
 AGAF|Afutara Airport|Afutara
