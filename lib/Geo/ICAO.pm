a110550e20dbb1c178f177ad8002427e80aad634
Jérôme Quelin <jquelin@gmail.com>
2009-11-28 16:27:16 +0100

    remove copyright mentions, will be added by dzil

--- lib/Geo/ICAO.pm
+++ lib/Geo/ICAO.pm
@@ -1,11 +1,3 @@
-#
-# This file is part of Geo::ICAO
-# Copyright (c) 2007 Jerome Quelin, all rights reserved.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the same terms as Perl itself.
-#
-
 package Geo::ICAO;
 
 use warnings;
